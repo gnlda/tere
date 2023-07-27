28fac207b2f7a6a7fdecdc5283b8fc5c4eea9ef8
gnlda <136723196+gnlda@users.noreply.github.com>
2023-07-27 19:53:56 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+const burgerButton = document.querySelector(".header__burger-btn");
+const burgerMenu = document.querySelector(".header__burger-menu");
+const body = document.querySelector("body");
+let opacity = burgerMenu.style.opacity;
+
+burgerButton.addEventListener("click", openMenu);
+
+let div = document.createElement('div');
+div.style.overflowY = 'scroll';
+div.style.width = '50px';
+div.style.height = '50px';
+document.body.append(div);
+let scrollWidth = div.offsetWidth - div.clientWidth;
+div.remove();
+
+function openMenu() {
+    if (opacity == 0) {
+        burgerMenu.style.opacity = "1";
+        burgerMenu.style.zIndex = "1";
+        opacity = 1;
+        body.style.overflow = "hidden";
+        body.style.paddingRight = `${scrollWidth}px`
+    } else {
+        burgerMenu.style.opacity= "0";
+        burgerMenu.style.zIndex = "-1";
+        opacity = 0;
+        body.style.overflow = "auto";
+        body.style.overflowX = "hidden";
+        body.style.paddingRight = "0px"
+    }
+}
